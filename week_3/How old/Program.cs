fe97edbdacd78323cf069e3b8bd3d6629bfb1234
DigitalForge <>
2020-12-05 14:00:53 +0100

    week 3

---
+++ week_3/How old/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace IleŻyjesz_Dane
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("Hej");
+                Console.Write("Przedstawisz się ? (imie) : ");
+                name = Console.ReadLine();
+            } while (name == string.Empty);
+
+            string location;
+            do
+            {
+                Console.Clear();
+                Console.Write("Skąd pochodzisz ? : ");
+                location = Console.ReadLine();
+            } while (location == string.Empty);
+
+            string buff;
+            DateTime data;
+            do
+            {
+                Console.Clear();
+                Console.Write("A którego się urodzięś ? (format: DD MM YYYY; separators: / . , - spacje) : ");
+                buff = Console.ReadLine();
+            } while (!DateTime.TryParse(buff, out data));
+
+            Console.Clear();
+
+            byte year;
+            bool birthday = false;
+
+            if (data.Month == DateTime.Now.Month)
+            {
+                if (data.Day == DateTime.Now.Day)
+                {
+                    birthday = true;
+                    year = Convert.ToByte(DateTime.Now.Year - data.Year);
+                }
+                else
+                {
+                    year = Convert.ToByte(data.Day < DateTime.Now.Day ? DateTime.Now.Year - data.Year : DateTime.Now.Year - data.Year - 1);
+                }
+            }
+            else
+            {
+                year = Convert.ToByte(data.Month < DateTime.Now.Month ? DateTime.Now.Year - data.Year : DateTime.Now.Year - data.Year - 1);
+            }
+
+            if (birthday)
+            {
+                Console.WriteLine($"Hej {name} pochodzisz z {location} i masz już dziś {year} lat");
+                Console.WriteLine("100 lat");
+            }
+            else
+            {
+                Console.WriteLine($"Hej {name} pochodzisz z {location} i masz {year} lat");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ week_3/What number 1-100/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace ZaganijLiczbe_1_100
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte rnd;
+            {
+                Random rand = new Random(DateTime.Now.Second);
+                rnd = Convert.ToByte(rand.Next(1,101));
+            }
+
+            Console.WriteLine("Pomyślałem o liczbe z zakresu od 1 do 100 :)");
+            Console.WriteLine("Proszę zgadnij jaka to liczba ;)");
+
+            string s_buff;
+            int b_buff;
+            uint licznik = 1;
+            while (true)
+            {
+                Console.Write("Podaj liczbę od 1 do 100 : ");
+                s_buff = Console.ReadLine();
+                Console.Clear();
+
+                if (int.TryParse(s_buff,out b_buff))
+                {
+                    if (b_buff > 100 || b_buff < 1)
+                    {
+                        Console.WriteLine("Przekroczono zakres");
+                    }
+                    else
+                    {
+                        if (b_buff == rnd)
+                        {
+                            Console.WriteLine($"Zagadłeś za {licznik} razem :)");
+                            Console.ReadKey();
+                            break;
+                        }
+                        else if (b_buff > rnd)
+                        {
+                            Console.WriteLine("Liczba za duża");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Liczba za mała");
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Nie ma takiej liczby");
+                }
+                licznik++;
+            }
+        }
+    }
+}
