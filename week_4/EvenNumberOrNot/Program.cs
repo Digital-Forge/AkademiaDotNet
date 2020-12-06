ea5d158109023905ff642a43b321be47e89dc180
Digital-Forge <jan.m.szkudlarek@gmail.com>
2020-12-06 20:29:49 +0100

    week4 homework1

---
+++ week_4/EvenNumberOrNot/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace EvenNumberOrNot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Liczba całkowita : ");
+            int num;
+
+            if (int.TryParse(Console.ReadLine(), out num))
+            {
+                if (num % 2 == 0)
+                {
+                    Console.WriteLine("liczba jest parzysta");
+                }
+                else
+                {
+                    Console.WriteLine("liczba nie jest parzysta");
+                }
+            }
+            else
+            {
+                Console.WriteLine("To nie jest liczba całkowita");
+            }
+            Console.ReadKey();
+        }
+    }
+}
