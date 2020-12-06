836cfed7accada7f6554e1508b000394b5947870
DigitalForge <>
2020-12-06 21:28:15 +0100

    week4 homework2

---
+++ week_4/FizzBuzz/FizzBuzz.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class FizzBuzz
+    {
+        public void work(int number)
+        {
+            bool exit = false;
+
+            if (number % 3 == 0)
+            {
+                Console.Write("Fizz");
+                exit = true;
+            }
+            if (number % 5 == 0)
+            {
+                Console.Write("Buzz");
+                exit = true;
+            }
+            if (!exit)
+            {
+                Console.Write(number);
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ week_4/FizzBuzz/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Liczba całkowita : ");
+            int num;
+            if (int.TryParse(Console.ReadLine(),out num))
+            {
+                Console.Clear();
+                FizzBuzz fizzbuzz = new FizzBuzz();
+                fizzbuzz.work(num);
+            }
+            else
+            {
+                Console.WriteLine("złe dane wejściowe");
+            }
+            Console.ReadKey();
+        }
+    }
+}
