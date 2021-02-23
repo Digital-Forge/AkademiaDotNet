bbe591d01a9c23c3b2d1d011f26902057c741bbc
Digital-Forge <jan.m.szkudlarek@gmail.com>
2021-02-23 23:54:10 +0100

    Finish week 6

---
+++ week_6/PictureBox/PictureBox/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿
+namespace PictureBox
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pbViewer = new System.Windows.Forms.PictureBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.chbSizeMode = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbViewer)).BeginInit();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pbViewer
+            // 
+            this.pbViewer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pbViewer.Location = new System.Drawing.Point(3, 3);
+            this.pbViewer.Name = "pbViewer";
+            this.pbViewer.Size = new System.Drawing.Size(483, 371);
+            this.pbViewer.TabIndex = 0;
+            this.pbViewer.TabStop = false;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnAdd.Location = new System.Drawing.Point(3, 3);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(195, 28);
+            this.btnAdd.TabIndex = 1;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnClear.Location = new System.Drawing.Point(204, 3);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(195, 28);
+            this.btnClear.TabIndex = 2;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.pbViewer, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(489, 417);
+            this.tableLayoutPanel1.TabIndex = 3;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 3;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 80F));
+            this.tableLayoutPanel2.Controls.Add(this.btnAdd, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.btnClear, 1, 0);
+            this.tableLayoutPanel2.Controls.Add(this.chbSizeMode, 2, 0);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 380);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 1;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(483, 34);
+            this.tableLayoutPanel2.TabIndex = 4;
+            // 
+            // chbSizeMode
+            // 
+            this.chbSizeMode.AutoSize = true;
+            this.chbSizeMode.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.chbSizeMode.Location = new System.Drawing.Point(405, 3);
+            this.chbSizeMode.Name = "chbSizeMode";
+            this.chbSizeMode.Size = new System.Drawing.Size(75, 28);
+            this.chbSizeMode.TabIndex = 3;
+            this.chbSizeMode.Text = "Auto Size";
+            this.chbSizeMode.UseVisualStyleBackColor = true;
+            this.chbSizeMode.CheckedChanged += new System.EventHandler(this.chbSizeMode_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(489, 417);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pbViewer)).EndInit();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbViewer;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.CheckBox chbSizeMode;
+    }
+}
+
---
+++ week_6/PictureBox/PictureBox/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PictureBox
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Size = new Size(Properties.Settings.Default.WindowWidth, Properties.Settings.Default.WindowHeight);
+            chbSizeMode.Checked = Properties.Settings.Default.AutoSize;
+            chbSizeMode_CheckedChanged();
+
+            try
+            {
+                pbViewer.Load(Properties.Settings.Default.PicturePath);
+                btnClear.Enabled = true;
+            }
+            catch
+            {
+                pbViewer.Image = new Bitmap(pbViewer.Size.Width,pbViewer.Size.Height);
+                btnClear.Enabled = false;
+            }
+            
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog dialog = new OpenFileDialog())
+            {
+                dialog.RestoreDirectory = true;
+                dialog.Filter = "all files (*.*)|*.*";
+
+                if (dialog.ShowDialog() == DialogResult.OK)
+                {
+                    try
+                    {
+                        pbViewer.Load(dialog.FileName);
+                        Properties.Settings.Default.PicturePath = dialog.FileName;
+                        Properties.Settings.Default.Save();
+                        btnClear.Enabled = true;
+                    }
+                    catch
+                    {
+                        if (pbViewer.Image != null)
+                        {
+                            pbViewer.Image.Dispose();
+                        }
+                        pbViewer.Image = new Bitmap(pbViewer.Size.Width, pbViewer.Size.Height);
+                        btnClear.Enabled = false;
+                    }
+                }
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            if (pbViewer.Image != null)
+            {
+                pbViewer.Image.Dispose();
+            }
+            pbViewer.Image = new Bitmap(pbViewer.Size.Width, pbViewer.Size.Height);
+            btnClear.Enabled = false;
+            Properties.Settings.Default.PicturePath = "";
+            Properties.Settings.Default.Save();
+        }
+
+        private void chbSizeMode_CheckedChanged(object sender = null, EventArgs e = null)
+        {
+            if (chbSizeMode.Checked)
+            {
+                pbViewer.SizeMode = PictureBoxSizeMode.StretchImage;
+            }
+            else
+            {
+                pbViewer.SizeMode = PictureBoxSizeMode.Normal;
+            }
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Properties.Settings.Default.AutoSize = chbSizeMode.Checked;
+            Properties.Settings.Default.WindowWidth = this.Size.Width;
+            Properties.Settings.Default.WindowHeight = this.Size.Height;
+            Properties.Settings.Default.Save();
+        }
+    }
+}
